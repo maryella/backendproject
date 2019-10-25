dbe08e44ed6673095bdb88b76590df67b690699b
rjmckenney <janellemckenney17@gmail.com>
2019-10-25 11:06:03 -0400

    adding users model

--- models/userModels.js
+++ models/userModels.js
@@ -1,14 +1,50 @@
 const db = require('./conn');
-
+const bcrypt = require('bcryptjs');
 class User {
     constructor(name, email, password, city, state, picture) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.city = city;
-        this.state = state
+        this.state = state;
         this.picture =  picture;
     }
+
+    checkPassword(hashedPassword) {
+        return bcrypt.compareSync(this.password, hashedPassword);
+    }
+    async login(){
+        try{
+            const response = await db.one
+            (`SELECT * FROM users WHERE email = $1;`,
+            [this.email]
+            );
+            const isValid = this.checkPassword(response.password);
+            console.log('is it valid?' , isValid);
+            
+            if (!!isValid) {
+                const {id, name } = response;
+                return {isValid, id, name };
+            } else {
+                return{ isValid };
+            }
+        }catch(error) {
+            return error.message;
+        }
+    
+    }
+
+    async save() {
+        try{
+            console.log('this is the save method', this.email); 
+            const response = db.one(`INSERT INTO users (name, email, password, city, state) VALUES ($1,$2,$3,$4,$5) RETURNING id;`,
+            [this.name, this.email, this.password, this.city, this.state]);
+            //RETURNINGid add a row, auto increment
+        }catch (error) {
+            return error.message;
+        }
+    }
+
     static async getAll () {
         try{
             const response = await db.any(`SELECT * FROM winery_reviews;`);
@@ -19,4 +55,5 @@ class User {
         }
     }
 }
+
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/userModels')
+const bcrypt = require('bcryptjs');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', async(req, res, next)  => {
+  res.render('template', {
+    locals: {
+      title: 'Sign Up'
+    },
+    partials: {
+      partial: 'partial-signup'
+    }
+  });
+});
+router.post('/signup', async (req, res, next) => {
+  console.log(req.body);
+  const { name, email, password, city, state} = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  const user = new userModel (name, email, hash, city, state,);
+  const addUser = await user.save();
+  if (addUser){
+    res.status(200).redirect("/users/login");
+  }
+  else {
+    res.status(500);
+  }
+  
 });
 
+router.get('/login', async(req, res, next) => {
+  res.render('template', {
+    locals: {
+      title: "Login"
+    },
+    partials: {
+      partial: 'partial-login'
+    }
+  });
+});
+// the post sends information to the right place, which is the database
+
+
+
+  router.post("/login", async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    const user = new userModel(null, email, password);
+  
+    const response = await user.login();
+    console.log(response);
+  
+    if (!!response.isValid) {
+        const { id, name } = response;
+        // req.session.is_logged_in = true;
+        // req.session.first_name = first_name;
+        // req.session.last_name = last_name;
+        // req.session.user_id = id;
+        res.status(200).redirect("/users/signup");
+    } else {
+        res.sendStatus(401);
+    }
+  });
+
+
+
+
+  
+
+  
 module.exports = router;
