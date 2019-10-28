a39072460f56b6ae8f0d1280cff893c113d8b71a
Mary Ella Hill <hillmaryella@gmail.com>
2019-10-28 14:18:13 -0400

    Working Product

--- routes/wineries.js
+++ routes/wineries.js
@@ -17,6 +17,21 @@ router.get("/", async function (req, res, next) {
           }
       )
 })
+router.get("/addwinery", async (req, res, next) => {
+  const { winery_id } = req.params;
+//    console.log("req param:", req.params)
+  const winery = await wineryModel.getById(winery_id);
+  const reviews = await wineryModel.reviewsById(winery_id);
+  res.render("template", {
+    locals: {
+      title: "Add Winery",
+      isLoggedIn: req.session.is_logged_in
+    },
+    partials: {
+      partial: "partial-addwinery"
+    }
+  })
+});
 
 router.post("/addwinery", async (req, res) => {
     const{ name, address, street, city, state, primary_vine, zip } = req.body;
